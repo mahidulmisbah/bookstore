5f37bc9db1d1d311f8a50e925883d0548a02781b
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-07-02 02:16:28 +0600

    add authors controller and partially manage how author page work

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-if(process.env.NODE_ENV !== 'production')
-{
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
@@ -7,21 +6,24 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.set('layout','layouts/layout')
+app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{
-    useUnifiedTopology:true,
-    useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
 
 const db = mongoose.connection
-db.on('error', error =>console.error(error))
-db.once('open',() => console.log('Conncection successful'))
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Conncection successful'))
 
-app.use('/',indexRouter)
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 app.listen(process.env.PORT || 3000)
