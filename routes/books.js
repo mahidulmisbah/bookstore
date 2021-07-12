17680632b0c396c3d02711a0e876e233f1a3d9a7
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-07-13 02:36:32 +0600

    show/edit/delete book

--- routes/books.js
+++ routes/books.js
@@ -37,6 +37,28 @@ router.get('/new', async (req,res) =>{
     addNewBookPage(res, new Book() )
 })
 
+// show specific book and related information
+router.get('/:id', async (req, res) => {
+    try {
+      const book = await Book.findById(req.params.id)
+                             .populate('author')
+                             .exec()
+      res.render('books/showBook', { book: book })
+    } catch {
+      res.redirect('/')
+    }
+  })
+
+// edit book route
+router.get('/:id/edit', async(req,res)=>{
+    try{
+        book = await Book.findById(req.params.id)
+        EditBookPage(res,book)
+    }catch{
+        res.redirect('/')
+    }
+})
+
 // create new book route(new book information save to database)
 router.post('/',async (req,res) =>{
     const book = new Book({
@@ -51,11 +73,76 @@ router.post('/',async (req,res) =>{
         const newBook = await book.save()
         res.redirect('books')
    }catch{
-        addNewBookPage(res, new Book(), true )
+        addNewBookPage(res, new Book())
    }
 })
 
-async function addNewBookPage(res,book,hasError = false)
+// update book 
+router.put('/:id',async (req, res) => {
+    let book
+
+    try{
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title,
+        book.author = req.body.author,
+        book.publishDate = new Date(req.body.publishDate),
+        book.pageCount = req.body.pageCount,
+        book.description = req.body.description
+        if(req.body.cover != null && req.body.cover !=='')
+        {
+            saveCover(book,req.body.cover)
+        }
+        await book.save();
+        res.redirect(`/books/${book.id}`)
+       
+    }catch
+    {
+        if(book != null){
+           EditBookPage(res,book,true)
+        console.log("hello ")}
+        else 
+        {
+            res.render('books/edit', {
+            book: book,
+            errorMessage: 'Updation error'}) 
+        }
+    }   
+})
+
+
+// delete a book
+router.delete('/:id', async(req,res) =>{
+    let book
+    try{
+        book = await Book.findById(req.params.id)
+        await book.remove()
+        res.redirect('/books')
+    }catch{
+        if(book!=null)
+        {
+            res.render('books/showBook',{
+                book:book,
+                errorMessage: 'Error in deleting book'
+            })
+        }
+        else {
+            res.redirect('/')
+        }
+        
+    }
+})
+
+async function addNewBookPage(res,book,hasError)
+{
+    AddOrEditBookPage(res,book,'new',hasError)
+}
+
+async function EditBookPage(res,book,hasError)
+{
+    AddOrEditBookPage(res,book,'edit',hasError)
+}
+
+async function AddOrEditBookPage(res,book,form,hasError = false)
 {
     try{
         const authors = await Author.find({})
@@ -63,19 +150,16 @@ async function addNewBookPage(res,book,hasError = false)
             authors: authors,
             book : book
         }
-        if(hasError) params.errorMessage = "Error in adding new book"
-        res.render('books/new',params)
-    }catch{
-        res.redirect('/books')
+        if(hasError) 
+        {
+            if(form == 'edit') params.errorMessage = "Error in Edit book"
+            else params.errorMessage = "Error in creating book"
+        }
+        res.render(`books/${form}`,params)
+    }catch (e){
+        console.log(e)
+        res.redirect('/')
     }
-}
-
-function removeBookCover(filename)
-{
-    fileSytem.unlink(path.join(uploadPath, filename),err => {
-        if(err)
-            console.error(err)
-    }) 
 }
 
 function saveCover(book, coverEncoded) {
